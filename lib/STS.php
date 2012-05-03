c1fbc1b2c1842600a648234b755c2fa100954a4a
Jens <mail@jensjaeger.com>
2012-05-03 22:58:30 +0200

    Fix from Patrick

--- lib/STS.php
+++ lib/STS.php
@@ -1,16 +1,14 @@
 <?php
-require_once('simple_html_dom.php');
+if (!class_exists ("simple_html_dom")) {
+    include_once("simple_html_dom.php");
+}
 class STS
 {
   public static function getAdd(){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://sign.js-ing.com/a.php");
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $ch_data = curl_exec($ch);
-    curl_close($ch);
-
-    return str_get_html($ch_data);
+    $url = "http://sign.js-ing.com/a.php";
+    $response = wp_remote_get($url);
+    $data = wp_remote_retrieve_body( $response  );
+    return str_get_html($data); 
   }
 }
 ?>
